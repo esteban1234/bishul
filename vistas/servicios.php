9e6ebd10692a44f5a25eb12adcedd0440bd3828c
esteban1234 <tva_libra@hotmail.com>
2017-01-23 13:25:16 -0600

    servicos listo

---
+++ vistas/servicios.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+
+
+<?php include 'encabezado2.php' ?>
+
+<body>
+
+<?php include 'menu2.php' ?>
+
+	<article class="titcontacto">
+		<article class="tituloapartado">
+			Servicios
+		</article>
+
+		<article class="submenu">
+			<li><a href="../index.php">INICIO &nbsp;/</a></li>
+			<li class="cambio">SERVICIOS</li>
+		</article>
+	</article>
+	
+
+	<article class="contenser">
+	<article class="servicios">
+	  	<article class="conten-precios">
+		    <article class="tabla-precios">
+		      	<h1 class="pricing-title">XV AÑOS</h1>
+		      	<article class="contencir">
+		   			<img class="img" src="../img/xvaños.jpg" alt="" height="250">		          
+		       	</article>
+		        <p class="diseñado">Incluye:</p>
+		        <p class="texto_tabla">Meseros (el numero de meseros depende del numero de invitados), 1 bartender, renta de mesas, sillas, amplia variedad de mantelería, vajilla, plaque, mesa de postres. Precios no incluyen IVA.</p>
+		        <a href="contacto.php" class="boton_tabla"><p class="contratar">CONTRATAR</p></a>
+		    </article>
+
+		    <article class="tabla-precios">
+		      	<h1 class="pricing-title">BODAS</h1>
+		      	<article class="contencir">
+		   			<img class="img" src="../img/boda.jpg" alt="" height="250">		          
+		       	</article>
+		        <p class="diseñado">Incluye:</p>
+		        <p class="texto_tabla">Meseros (el numero de meseros depende del numero de invitados), 2 bartenders, renta de mesas, sillas, amplia variedad de mantelería, vajilla, plaque, mesa de postres. Precios no incluyen IVA.</p>
+		        <a href="contacto.php" class="boton_tabla"><p class="contratar">CONTRATAR</p></a>
+		    </article>
+
+		    <article class="tabla-precios">
+		      	<h1 class="pricing-title">GRADUACIONES</h1>
+		      	<article class="contencir">
+		   			<img class="img" src="../img/gradu3.jpg" alt="" height="250">		          
+		       	</article>
+		        <p class="diseñado">Incluye:</p>
+		        <p class="texto_tabla">Meseros (el numero de meseros depende del numero de invitados), 3 bartenders, renta de mesas, sillas, amplia variedad de mantelería, vajilla, plaque, mesa de postres. Precios no incluyen IVA.</p>
+		        <a href="contacto.php" class="boton_tabla"><p class="contratar">CONTRATAR</p></a>
+		    </article>  
+	  </article>
+
+		<article class="conten-precios">
+		    <article class="tabla-precios">
+		      	<h1 class="pricing-title">BAUTIZOS</h1>
+		      	<article class="contencir">
+		   			<img class="img" src="../img/bautizo.jpg" alt="" height="250">		          
+		       	</article>
+		        <p class="diseñado">Incluye:</p>
+		        <p class="texto_tabla">Meseros (el numero de meseros depende del numero de invitados), renta de mesas, sillas, amplia variedad de mantelería, vajilla, mesa de postres. Precios no incluyen IVA.</p>
+		        <a href="contacto.php" class="boton_tabla"><p class="contratar">CONTRATAR</p></a>
+		    </article>
+
+		    <article class="tabla-precios">
+		      	<h1 class="pricing-title">PRIMERA COMUNION</h1>
+		      	<article class="contencir">
+		   			<img class="img" src="../img/primera.jpg" alt="" height="250">		          
+		       	</article>
+		        <p class="diseñado">Incluye:</p>
+		        <p class="texto_tabla">Meseros (el numero de meseros depende del numero de invitados), renta de mesas, sillas, amplia variedad de mantelería, vajilla, mesa de postres. Precios no incluyen IVA.</p>
+		        <a href="contacto.php" class="boton_tabla"><p class="contratar">CONTRATAR</p></a>
+		    </article>
+
+		    <article class="tabla-precios">
+		      	<h1 class="pricing-title">SERVICIOS GENERALES</h1>
+		      	<article class="contencir">
+		   			<img class="img" src="../img/lujo.jpg" alt="" height="250">		          
+		       	</article>
+		        <p class="diseñado">Incluye:</p>
+		        <p class="texto_tabla">
+		        * Desayuno de lujo <br>
+		        * Almuerzo de lujo <br>
+		        * Cena de lujo <br>
+		        * Barra de Cocteles <br>
+		        * Muebles Lounge, vintage…
+		        </p>
+		        <a href="contacto.php" class="boton_tabla"><p class="contratar">CONTRATAR</p></a>
+		    </article>  
+	  </article>
+
+
+	</article>
+</article>
+
+<article class="contentextser">
+	<p>Te ayudamos en la asesoría completa o en los servicios</p>
+</article>
+	
+<!-- <p class="btnser"><a class="pulse" href="contacto.php">CONTRATAR</a></p> -->
+
+		<?php include 'pie.php' ?>
+
+<span class="ir-arriba icon-chevron-up"></span>
+<script type="text/javascript" src="../js/jquery.js"></script>
+<script src="../js/active.js"></script>
+<script src="../js/lightbox.js"></script>
+<script src="../js/bootstrap.js"></script>
+<script type="text/javascript" src="../js/iconoarriba.js"></script>
+
+</body>
+</html>
